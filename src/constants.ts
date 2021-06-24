55bb1b7458058901614d3ea116129f6fc3b5d3da
Dome C <dome@tel.co.th>
2021-06-24 18:22:54 +0700

    first commit

---
+++ src/constants.ts
@@ -0,0 +1,269 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 1,
+  ROPSTEN = 3,
+  RINKEBY = 4,
+  GÖRLI = 5,
+  KOVAN = 42,
+  MATIC = 137,
+  MATIC_TESTNET = 80001,
+  FANTOM = 250,
+  FANTOM_TESTNET = 4002,
+  XDAI = 100,
+  BKC = 96,
+  BSC = 56,
+  BSC_TESTNET = 97,
+  ARBITRUM = 79377087078960,
+  MOONBASE = 1287,
+  AVALANCHE = 43114,
+  FUJI = 43113,
+  HECO = 128,
+  HECO_TESTNET = 256,
+  HARMONY = 1666600000,
+  HARMONY_TESTNET = 1666700000,
+  OKEX = 66,
+  OKEX_TESTNET = 65
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+/* 
+BSC
+reusing "UniswapV2Factory" at 0x03387a19B3F9887753C1edd5d9075daAaD31b059
+reusing "UniswapV2Router02" at 0x2E9F7557351689a6Da8ebB0A4fAE94995163Cf75
+reusing "SushiToken" at 0x56dBe1eE34Ab7b54AdE7238fdA171611C3102EB9
+reusing "MasterChef" at 0x1E9C1cbA0532C747b58c4a684e3db4FB4D10C7A7
+reusing "MiniChefV2" at 0x0a1208359f4Fe080a4dCd81A354b908Db5F7A9cA
+reusing "SushiBar" at 0xa2b07c4966a801F1D623Acb642a44C78554c5593
+reusing "SushiMaker" at 0x0F9CBc0C86C183f15271B720276D162d8cBfA225
+        "SushiRoll" at 0x95F191b513032F46444E2Ff973F38454f5353773 
+BSC Testnet
+reusing "UniswapV2Factory" at 0xA9a60E56b6AdfB41cF1d1BFD37329e4B9c4e7d32
+reusing "UniswapV2Router02" at 0xc8F145fD17669A2De1573CB3Dfb77e33A51E9a7C
+reusing "SushiToken" at 0xD1244D4Cf375d8CDB9c3e94B16A65F3E8F092e79
+reusing "MasterChef" at 0xe759F268862a5D6E8EfF26bD58B8Fc4921b89A04
+reusing "MiniChefV2" at 0x1F98aBbDAC4C3D9fd4064D3330552aAFB6193D57
+reusing "SushiBar" at 0xB04a7F648dd93edcF5fc15c2468e5b6AcF41127C
+reusing "SushiMaker" at 0x284A6E955dfaD6b43Fb88Dc66ad6605Db1C33Cd0
+*/
+
+//export const INIT_CODE_HASH: string = '0xc00c331afe5428543f3288d7358c4809f759b94d8dabb493a8e228aef0d091ee'
+export const INIT_CODE_HASH: string = '0xa6fb3b505df6ebcc80333dff1d670c7fa7a6d945ac8d5594215131261ac07f6d'
+// 0x7d8445494f719b84bf2eb61c3de7eeb99d69ebd7efd26ce57aeef1ad7c55779c
+
+export const FACTORY_ADDRESS: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac',
+  [ChainId.ROPSTEN]: '0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
+  [ChainId.RINKEBY]: '0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
+  [ChainId.GÖRLI]: '0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
+  [ChainId.KOVAN]: '0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
+  [ChainId.FANTOM]: '0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
+  [ChainId.FANTOM_TESTNET]: '',
+  [ChainId.MATIC]: '0xFFDDfc8A5A30c36e0F0C9CC3b59b214323492d9a',
+  [ChainId.MATIC_TESTNET]: '0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
+  [ChainId.XDAI]: '0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
+  [ChainId.BKC]: '0x5ba4A2f5F50A596bba9191e22026Dd48fd4a400A',
+  [ChainId.BSC]: '0x4dD1d9Dd0f3d9C3F6F747F99928C29924A1b42b1',
+  [ChainId.BSC_TESTNET]: '0xA9a60E56b6AdfB41cF1d1BFD37329e4B9c4e7d32',
+  [ChainId.ARBITRUM]: '',
+  [ChainId.MOONBASE]: '0x2Ce3F07dD4c62b56a502E223A7cBE38b1d77A1b5',
+  [ChainId.AVALANCHE]: '0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
+  [ChainId.FUJI]: '0xd00ae08403B9bbb9124bB305C09058E32C39A48c',
+  [ChainId.HECO]: '0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
+  [ChainId.HECO_TESTNET]: '0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
+  [ChainId.HARMONY]: '0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
+  [ChainId.HARMONY_TESTNET]: '0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
+  [ChainId.OKEX]: '0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
+  [ChainId.OKEX_TESTNET]: '0xc35DADB65012eC5796536bD9864eD8773aBc74C4'
+}
+
+export const ROUTER_ADDRESS: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F',
+  [ChainId.RINKEBY]: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506',
+  [ChainId.ROPSTEN]: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506',
+  [ChainId.GÖRLI]: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506',
+  [ChainId.KOVAN]: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506',
+  [ChainId.FANTOM]: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506',
+  [ChainId.FANTOM_TESTNET]: '',
+  [ChainId.MATIC]: '0x8BF712d9906e6E77461dAea19B52001cBa57f372',
+  [ChainId.MATIC_TESTNET]: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506',
+  [ChainId.XDAI]: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506',
+  [ChainId.BKC]: '0xFFDDfc8A5A30c36e0F0C9CC3b59b214323492d9a',
+  [ChainId.BSC]: '0x5ba4A2f5F50A596bba9191e22026Dd48fd4a400A',
+  [ChainId.BSC_TESTNET]: '0xc8F145fD17669A2De1573CB3Dfb77e33A51E9a7C',
+  [ChainId.ARBITRUM]: '',
+  [ChainId.MOONBASE]: '0xeB5c2BB5E83B51d83F3534Ae21E84336B8B376ef',
+  [ChainId.AVALANCHE]: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506',
+  [ChainId.FUJI]: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506',
+  [ChainId.HECO]: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506',
+  [ChainId.HECO_TESTNET]: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506',
+  [ChainId.HARMONY]: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506',
+  [ChainId.HARMONY_TESTNET]: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506',
+  [ChainId.OKEX]: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506',
+  [ChainId.OKEX_TESTNET]: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506'
+}
+
+export const SUSHI_ADDRESS: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0x6B3595068778DD592e39A122f4f5a5cF09C90fE2',
+  [ChainId.ROPSTEN]: '0x0769fd68dFb93167989C6f7254cd0D766Fb2841F',
+  [ChainId.RINKEBY]: '0x0769fd68dFb93167989C6f7254cd0D766Fb2841F',
+  [ChainId.GÖRLI]: '0x0769fd68dFb93167989C6f7254cd0D766Fb2841F',
+  [ChainId.KOVAN]: '0x0769fd68dFb93167989C6f7254cd0D766Fb2841F',
+  [ChainId.FANTOM]: '',
+  [ChainId.FANTOM_TESTNET]: '',
+  [ChainId.MATIC]: '',
+  [ChainId.MATIC_TESTNET]: '',
+  [ChainId.XDAI]: '',
+  [ChainId.BKC]: '',
+  [ChainId.BSC]: '',
+  [ChainId.BSC_TESTNET]: '0xD1244D4Cf375d8CDB9c3e94B16A65F3E8F092e79',
+  [ChainId.ARBITRUM]: '',
+  [ChainId.MOONBASE]: '',
+  [ChainId.AVALANCHE]: '',
+  [ChainId.FUJI]: '',
+  [ChainId.HECO]: '',
+  [ChainId.HECO_TESTNET]: '',
+  [ChainId.HARMONY]: '',
+  [ChainId.HARMONY_TESTNET]: '',
+  [ChainId.OKEX]: '',
+  [ChainId.OKEX_TESTNET]: ''
+}
+
+export const MASTERCHEF_ADDRESS: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0xc2EdaD668740f1aA35E4D8f227fB8E17dcA888Cd',
+  [ChainId.ROPSTEN]: '0x80C7DD17B01855a6D2347444a0FCC36136a314de',
+  [ChainId.RINKEBY]: '0x80C7DD17B01855a6D2347444a0FCC36136a314de',
+  [ChainId.GÖRLI]: '0x80C7DD17B01855a6D2347444a0FCC36136a314de',
+  [ChainId.KOVAN]: '0x80C7DD17B01855a6D2347444a0FCC36136a314de',
+  [ChainId.FANTOM]: '',
+  [ChainId.FANTOM_TESTNET]: '',
+  [ChainId.MATIC]: '',
+  [ChainId.MATIC_TESTNET]: '',
+  [ChainId.XDAI]: '',
+  [ChainId.BKC]: '',
+  [ChainId.BSC]: '',
+  [ChainId.BSC_TESTNET]: '0xe759F268862a5D6E8EfF26bD58B8Fc4921b89A04',
+  [ChainId.ARBITRUM]: '',
+  [ChainId.MOONBASE]: '',
+  [ChainId.AVALANCHE]: '',
+  [ChainId.FUJI]: '',
+  [ChainId.HECO]: '',
+  [ChainId.HECO_TESTNET]: '',
+  [ChainId.HARMONY]: '',
+  [ChainId.HARMONY_TESTNET]: '',
+  [ChainId.OKEX]: '',
+  [ChainId.OKEX_TESTNET]: ''
+}
+
+export const BAR_ADDRESS: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0x8798249c2E607446EfB7Ad49eC89dD1865Ff4272',
+  [ChainId.ROPSTEN]: '0x1be211D8DA40BC0ae8719c6663307Bfc987b1d6c',
+  [ChainId.RINKEBY]: '0x1be211D8DA40BC0ae8719c6663307Bfc987b1d6c',
+  [ChainId.GÖRLI]: '0x1be211D8DA40BC0ae8719c6663307Bfc987b1d6c',
+  [ChainId.KOVAN]: '0x1be211D8DA40BC0ae8719c6663307Bfc987b1d6c',
+  [ChainId.FANTOM]: '',
+  [ChainId.FANTOM_TESTNET]: '',
+  [ChainId.MATIC]: '',
+  [ChainId.MATIC_TESTNET]: '',
+  [ChainId.XDAI]: '',
+  [ChainId.BSC]: '',
+  [ChainId.BKC]: '',
+  [ChainId.BSC_TESTNET]: '0xB04a7F648dd93edcF5fc15c2468e5b6AcF41127C',
+  [ChainId.ARBITRUM]: '',
+  [ChainId.MOONBASE]: '',
+  [ChainId.AVALANCHE]: '',
+  [ChainId.FUJI]: '',
+  [ChainId.HECO]: '',
+  [ChainId.HECO_TESTNET]: '',
+  [ChainId.HARMONY]: '',
+  [ChainId.HARMONY_TESTNET]: '',
+  [ChainId.OKEX]: '',
+  [ChainId.OKEX_TESTNET]: ''
+}
+
+export const MAKER_ADDRESS: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0xE11fc0B43ab98Eb91e9836129d1ee7c3Bc95df50',
+  [ChainId.ROPSTEN]: '0x1b9d177CcdeA3c79B6c8F40761fc8Dc9d0500EAa',
+  [ChainId.RINKEBY]: '0x1b9d177CcdeA3c79B6c8F40761fc8Dc9d0500EAa',
+  [ChainId.GÖRLI]: '0x1b9d177CcdeA3c79B6c8F40761fc8Dc9d0500EAa',
+  [ChainId.KOVAN]: '0x1b9d177CcdeA3c79B6c8F40761fc8Dc9d0500EAa',
+  [ChainId.FANTOM]: '',
+  [ChainId.FANTOM_TESTNET]: '',
+  [ChainId.MATIC]: '',
+  [ChainId.MATIC_TESTNET]: '',
+  [ChainId.XDAI]: '',
+  [ChainId.BKC]: '',
+  [ChainId.BSC]: '',
+  [ChainId.BSC_TESTNET]: '0x284A6E955dfaD6b43Fb88Dc66ad6605Db1C33Cd0',
+  [ChainId.ARBITRUM]: '',
+  [ChainId.MOONBASE]: '',
+  [ChainId.AVALANCHE]: '',
+  [ChainId.FUJI]: '',
+  [ChainId.HECO]: '',
+  [ChainId.HECO_TESTNET]: '',
+  [ChainId.HARMONY]: '',
+  [ChainId.HARMONY_TESTNET]: '',
+  [ChainId.OKEX]: '',
+  [ChainId.OKEX_TESTNET]: ''
+}
+
+export const TIMELOCK_ADDRESS: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0x9a8541Ddf3a932a9A922B607e9CF7301f1d47bD1',
+  [ChainId.ROPSTEN]: '',
+  [ChainId.RINKEBY]: '',
+  [ChainId.GÖRLI]: '',
+  [ChainId.KOVAN]: '',
+  [ChainId.FANTOM]: '',
+  [ChainId.FANTOM_TESTNET]: '',
+  [ChainId.MATIC]: '',
+  [ChainId.MATIC_TESTNET]: '',
+  [ChainId.XDAI]: '',
+  [ChainId.BKC]: '',
+  [ChainId.BSC]: '',
+  [ChainId.BSC_TESTNET]: '',
+  [ChainId.ARBITRUM]: '',
+  [ChainId.MOONBASE]: '',
+  [ChainId.AVALANCHE]: '',
+  [ChainId.FUJI]: '',
+  [ChainId.HECO]: '',
+  [ChainId.HECO_TESTNET]: '',
+  [ChainId.HARMONY]: '',
+  [ChainId.HARMONY_TESTNET]: '',
+  [ChainId.OKEX]: '',
+  [ChainId.OKEX_TESTNET]: ''
+}
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
